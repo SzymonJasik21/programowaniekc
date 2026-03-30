dfb16962f5da89681258f92a302d5d1f706d3461
Szymon Jasik <szymon.jasik@op.pl>
2026-03-30 19:50:51 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import  Flask, render_template, request 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "flask dziala"
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    fullname = None
+    error = None 
+    if request.method == "POST":
+        fullname = request.form.get('fullname')
+        if not fullname.strip():
+            error = "Nie podales swojego imienia i nazwiska"
+            return render_template("form.html", fullname = None, error = error)
+    return render_template("form.html", fullname = fullname)
+
+@app.route("/calc", methods=["GET","POST"])
+def calc():
+    result = None 
+    error = None 
+    if request.method =="POST":
+        a = request.form.get("a")
+        b = request.form.get("b")
+        operator = request.form.get("operator")
+
+        if not a or not b:
+            error = "nie podales liczb"
+            return render_template("form.html", result = None, error = error, a=a, b=b, operator = operator)
+
+        if operator == "/" and b=="0":
+            error = "Nie wolno dzielic przez 0"
+            return render_template("calc.html", result = None, error = error, a=a, b=b, operator = operator)
+        
+        result = eval(f"{a}{operator}{b}")
+
+    return render_template("calc.html", result=result , error = error)
+
+app.run(debug=True)
